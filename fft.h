6ea7709e2880c4afbe6363a8100a5605c95fdc1d
Michael Bloom <mike.r.bloom@protonmail.com>
2023-06-09 19:14:23 -0400

    Fix warnings

--- fft.h
+++ fft.h
@@ -6,10 +6,10 @@
 
 //determine highest bit and return its position
 //std method is not constexpr so thats why this was created
-constexpr unsigned int fft_log2(unsigned int num)
+constexpr uint fft_log2(uint num)
 {
-    unsigned int retValue {0};
-    while(num = num >> 1) {
+    uint retValue {0};
+    while( (num = num >> 1) > 0) {
         retValue++;
     }
     return retValue;
@@ -20,7 +20,7 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_cosines_bad()
 {
     std::array<std::array<double, N>, fft_log2(N)> cosines {0};
     for (size_t i = 0; i < cosines.size(); i++) {
-        unsigned int pow2 = 1 << i + 1;
+        uint pow2 = 1 << i + 1;
         for (size_t j = 0; j < cosines.at(i).size(); j++) {
             cosines.at(i).at(j) = std::cos(2 * M_PI * j / pow2);
         }
@@ -33,7 +33,7 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_cosines()
 {
     std::array<std::array<double, N>, fft_log2(N)> cosines {0};
     for (size_t i = 0; i < cosines.size(); i++) {
-        unsigned int pow2 = 1 << i + 1;
+        uint pow2 = 1 << (i + 1);
         
         //first element is always 1
         cosines.at(i).at(0) = 1.0;
@@ -47,8 +47,8 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_cosines()
             }
         }
 
-        //calculate up to < 90
         else {
+            //calculate up to but not including 90 deg
             uint num = 1 << (i - 1);
             for (size_t j = 1; j < num; j++) {
                 cosines.at(i).at(j) = std::cos(2 * M_PI * j / pow2);
@@ -82,7 +82,7 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_sines_bad()
 {
     std::array<std::array<double, N>, fft_log2(N)> sines {0};
     for (size_t i = 0; i < sines.size(); i++) {
-        unsigned int pow2 = 1 << i + 1;
+        uint pow2 = 1 << i + 1;
         for (size_t j = 0; j < sines.at(i).size(); j++) {
             sines.at(i).at(j) = -std::sin(2 * M_PI * j / pow2);
         }
@@ -95,7 +95,7 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_sines()
 {
     std::array<std::array<double, N>, fft_log2(N)> sines {0};
     for (size_t i = 0; i < sines.size(); i++) {
-        unsigned int pow2 = 1 << i + 1;
+        uint pow2 = 1 << (i + 1);
         
         //first element is always 0
         sines.at(i).at(0) = 0.0;
@@ -104,8 +104,8 @@ constexpr std::array<std::array<double, N>, fft_log2(N)> calc_sines()
         //i = 0 is special
         //all values are 0
 
-        //calculate up to < 90
         if (i > 0) {
+            //calculate up to but not include 90 deg
             uint num = 1 << (i - 1);
             for (size_t j = 1; j < num; j++) {
                 sines.at(i).at(j) = -std::sin(2 * M_PI * j / pow2);
@@ -152,7 +152,7 @@ constexpr std::array<std::array<std::complex<double>, N>, fft_log2(N)> calc_wCoe
 }
 
 //determine if num is a power of 2
-constexpr bool isPowerOf2(unsigned int num)
+constexpr bool isPowerOf2(uint num)
 {
     if (num == 0)
         return false;
@@ -190,7 +190,7 @@ constexpr std::array<uint, N> calc_swap_lookup()
         swapLookup.at(i) = reverse<N>(i);
     }
     //then go through one more time and for every pair, unreverse the one with the higher index
-    //this prevents the swap from occuring twice, effectively undoing the swap
+    //this prevents the swap from occuring twice, which would undo the swap
     for (uint i = 1; i < N - 1; i++) {
         uint i2 = swapLookup.at(i);
         if (i2 != i) {
@@ -218,15 +218,15 @@ void fft(std::array<std::complex<double>, N>& data) {
 
     //outer most loop is the FFT stages
     //2pt FFT -> 4pt FFT -> 8pt FFT -> etc
-    int i2 = 0;
-    for (int i = 1; i < N; i = i << 1) {
+    uint i2 = 0;
+    for (uint i = 1; i < N; i = i << 1) {
 
         //the inside 2 loops perform the butterfly pattern
-        for (int j = 0; j < N; j += (i << 1)) {
+        for (uint j = 0; j < N; j += (i << 1)) {
 
-            for (int k = 0; k < i; k++) {
-                int index1 = j + k;
-                int index2 = j + k + i;
+            for (uint k = 0; k < i; k++) {
+                uint index1 = j + k;
+                uint index2 = j + k + i;
                 std::complex<double> val1 = data.at(index1) + data.at(index2) * wCoeffs.at(i2).at(index1);
                 std::complex<double> val2 = data.at(index1) + data.at(index2) * wCoeffs.at(i2).at(index2);
                 data.at(index1) = val1;

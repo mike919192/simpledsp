52f845d32b6a5f6c52dc041558981e2cf8d29e3f
Michael Bloom <mike.r.bloom@protonmail.com>
2023-07-16 14:22:11 -0400

    Added pretty good test coverage for radix 2 and radix 4

--- testFFT.cpp
+++ testFFT.cpp
@@ -2,6 +2,18 @@
 #include "catch2/catch_amalgamated.hpp"
 #include "fft.h"
 
+template <size_t N>
+double calc_max_error(const complex_array<double, N> &observed, const complex_array<double, N> &expected)
+{
+    std::array<double, N> error {0};
+
+    for (size_t i = 0; i < error.size(); i++) {
+        error.at(i) = std::abs(observed.at(i) - expected.at(i));
+    }
+
+    return *std::max_element(error.begin(), error.end());
+}
+
 TEST_CASE("FFT Radix 2 test", "[single-file]")
 {
     constexpr uint N {64};
@@ -20,13 +32,7 @@ TEST_CASE("FFT Radix 2 test", "[single-file]")
     {
         fft(s);
 
-        std::array<double, N> error {0};
-
-        for (size_t i = 0; i < error.size(); i++) {
-            error.at(i) = std::abs(S.at(i) - s.at(i));
-        }
-
-        double max_error = *std::max_element(error.begin(), error.end());
+        double max_error = calc_max_error(S, s);
 
         REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
     }
@@ -35,16 +41,87 @@ TEST_CASE("FFT Radix 2 test", "[single-file]")
     {        
         fft<reverse_fft>(S);
 
-        std::array<double, N> error {0};
-
-        for (size_t i = 0; i < error.size(); i++) {
-            error.at(i) = std::abs(S.at(i) - s.at(i));
-        }
-
-        double max_error = *std::max_element(error.begin(), error.end());
+        double max_error = calc_max_error(S, s);
 
         REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
     }
+
+    SECTION("FFT Time shift test")
+    {
+        //s2 shifted 90 degrees from s
+        complex_array<double, N> s2 {0};
+    
+        for (size_t i = 0; i < s2.size(); i++) {
+            s2.at(i) = std::cos(n * 2 * M_PI * i / N + (M_PI / 2.0));
+        }
+
+        fft(s);
+        fft(s2);
+
+        complex_array<double, N> S2 {0};
+        S2.at(n) = std::complex<double>(0, N / 2);
+        S2.at(N - n) = std::complex<double>(0, -(N / 2.0));
+
+        double max_error = calc_max_error(S2, s2);
+        REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
+    }
+}
+
+TEST_CASE("FFT Radix 2 linearity", "[single-file]")
+{
+    constexpr double freq1 {1000.0};
+    constexpr double freq2 {500.0};
+    constexpr double fs {8000.0};
+    constexpr double a1 {1.5};
+    constexpr double a2 {2.5};
+    constexpr uint N {256};
+
+    //test linearity
+    //FFT(a1x1[n]+a2x2[n])=a1FFT(x1[n])+a2FFT(x2[n])
+
+    complex_array<double, N> x1 {0};
+    complex_array<double, N> x2 {0};
+    for (size_t i = 0; i < x1.size(); i++) {
+        double value = std::sin(2.0 * M_PI * freq1 * (1.0 / fs) * i);
+        x1.at(i) = std::complex<double>(value, 0);
+        
+        double value2 = std::sin(2.0 * M_PI * freq2 * (1.0 / fs) * i);
+        x2.at(i) = std::complex<double>(value2, 0);
+    }
+
+    complex_array<double, N> a1x1 = x1;
+    std::for_each(a1x1.begin(), a1x1.end(), [a1](auto &n) { n *= a1; });
+
+    complex_array<double, N> a2x2 = x2;
+    std::for_each(a2x2.begin(), a2x2.end(), [a2](auto &n) { n *= a2; });
+
+    complex_array<double, N> a1x1_plus_a2x2 {0};
+    for (size_t i = 0; i < a1x1_plus_a2x2.size(); i++) {
+        a1x1_plus_a2x2.at(i) = a1x1.at(i) + a2x2.at(i);
+    }
+
+    complex_array<double, N> fft_data1 = a1x1_plus_a2x2;
+    fft(fft_data1);
+
+    complex_array<double, N> fft_data2 = x1;
+    complex_array<double, N> fft_data3 = x2;
+    fft(fft_data2);
+    fft(fft_data3);
+
+    complex_array<double, N> a1_fft_data2 = fft_data2;
+    std::for_each(a1_fft_data2.begin(), a1_fft_data2.end(), [a1](auto &n) { n *= a1; });
+
+    complex_array<double, N> a2_fft_data3 = fft_data3;
+    std::for_each(a2_fft_data3.begin(), a2_fft_data3.end(), [a2](auto &n) { n *= a2; });
+
+    complex_array<double, N> a1_fft_data2_plus_a2_fft_data3 {0};
+    for (size_t i = 0; i < a1_fft_data2_plus_a2_fft_data3.size(); i++) {
+        a1_fft_data2_plus_a2_fft_data3.at(i) = a1_fft_data2.at(i) + a2_fft_data3.at(i);
+    }
+
+    //difference should be very close to 0
+    double max_error = calc_max_error(fft_data1, a1_fft_data2_plus_a2_fft_data3);
+    REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
 }
 
 TEST_CASE("FFT Radix 4 test", "[single-file]")
@@ -65,13 +142,7 @@ TEST_CASE("FFT Radix 4 test", "[single-file]")
     {
         fft_radix4(s);
 
-        std::array<double, N> error {0};
-
-        for (size_t i = 0; i < error.size(); i++) {
-            error.at(i) = std::abs(S.at(i) - s.at(i));
-        }
-
-        double max_error = *std::max_element(error.begin(), error.end());
+        double max_error = calc_max_error(S, s);
 
         REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
     }
@@ -80,16 +151,87 @@ TEST_CASE("FFT Radix 4 test", "[single-file]")
     {        
         fft_radix4<reverse_fft>(S);
 
-        std::array<double, N> error {0};
-
-        for (size_t i = 0; i < error.size(); i++) {
-            error.at(i) = std::abs(S.at(i) - s.at(i));
-        }
-
-        double max_error = *std::max_element(error.begin(), error.end());
+        double max_error = calc_max_error(S, s);
 
         REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
     }
+
+    SECTION("FFT Time shift test")
+    {
+        //s2 shifted 90 degrees from s
+        complex_array<double, N> s2 {0};
+    
+        for (size_t i = 0; i < s2.size(); i++) {
+            s2.at(i) = std::cos(n * 2 * M_PI * i / N + (M_PI / 2.0));
+        }
+
+        fft_radix4(s);
+        fft_radix4(s2);
+
+        complex_array<double, N> S2 {0};
+        S2.at(n) = std::complex<double>(0, N / 2);
+        S2.at(N - n) = std::complex<double>(0, -(N / 2.0));
+
+        double max_error = calc_max_error(S2, s2);
+        REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
+    }
+}
+
+TEST_CASE("FFT Radix 4 linearity", "[single-file]")
+{
+    constexpr double freq1 {1000.0};
+    constexpr double freq2 {500.0};
+    constexpr double fs {8000.0};
+    constexpr double a1 {1.5};
+    constexpr double a2 {2.5};
+    constexpr uint N {256};
+
+    //test linearity
+    //FFT(a1x1[n]+a2x2[n])=a1FFT(x1[n])+a2FFT(x2[n])
+
+    complex_array<double, N> x1 {0};
+    complex_array<double, N> x2 {0};
+    for (size_t i = 0; i < x1.size(); i++) {
+        double value = std::sin(2.0 * M_PI * freq1 * (1.0 / fs) * i);
+        x1.at(i) = std::complex<double>(value, 0);
+        
+        double value2 = std::sin(2.0 * M_PI * freq2 * (1.0 / fs) * i);
+        x2.at(i) = std::complex<double>(value2, 0);
+    }
+
+    complex_array<double, N> a1x1 = x1;
+    std::for_each(a1x1.begin(), a1x1.end(), [a1](auto &n) { n *= a1; });
+
+    complex_array<double, N> a2x2 = x2;
+    std::for_each(a2x2.begin(), a2x2.end(), [a2](auto &n) { n *= a2; });
+
+    complex_array<double, N> a1x1_plus_a2x2 {0};
+    for (size_t i = 0; i < a1x1_plus_a2x2.size(); i++) {
+        a1x1_plus_a2x2.at(i) = a1x1.at(i) + a2x2.at(i);
+    }
+
+    complex_array<double, N> fft_data1 = a1x1_plus_a2x2;
+    fft_radix4(fft_data1);
+
+    complex_array<double, N> fft_data2 = x1;
+    complex_array<double, N> fft_data3 = x2;
+    fft_radix4(fft_data2);
+    fft_radix4(fft_data3);
+
+    complex_array<double, N> a1_fft_data2 = fft_data2;
+    std::for_each(a1_fft_data2.begin(), a1_fft_data2.end(), [a1](auto &n) { n *= a1; });
+
+    complex_array<double, N> a2_fft_data3 = fft_data3;
+    std::for_each(a2_fft_data3.begin(), a2_fft_data3.end(), [a2](auto &n) { n *= a2; });
+
+    complex_array<double, N> a1_fft_data2_plus_a2_fft_data3 {0};
+    for (size_t i = 0; i < a1_fft_data2_plus_a2_fft_data3.size(); i++) {
+        a1_fft_data2_plus_a2_fft_data3.at(i) = a1_fft_data2.at(i) + a2_fft_data3.at(i);
+    }
+
+    //difference should be very close to 0
+    double max_error = calc_max_error(fft_data1, a1_fft_data2_plus_a2_fft_data3);
+    REQUIRE(max_error < 4 * N * std::numeric_limits<double>::epsilon());
 }
 
 TEST_CASE("FFT benchmark", "[single-file]")
